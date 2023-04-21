b2fa28b8a4b2a5214cfc50833cf67d3ad267d5c9
Mayank Solanki <83648453+spike-spiegel-21@users.noreply.github.com>
2023-04-21 14:23:55 +0530

    Update DistributedManufacturing.sol
    Added a ProductStatus event to be emitted when the product stage is updated.
    Added a modifier onlyOwner to restrict certain functions to the product owner.
    Added a getProductDetails() function to view product details more easily.
    Replaced the for loop in addProduct() function with a helper function to improve readability.
    Refactored the sendNextMachine() function to reduce gas usage and improve readability.

--- contracts/DistributedManufacturing.sol
+++ contracts/DistributedManufacturing.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
 
@@ -21,9 +21,18 @@ contract DistributedManufacturing {
     }
 
     uint256 public productCounter;
-
     mapping(uint256 => ProductDetails) public productDetailsMap;
     uint256[5] public rateChart;
+
+    event ProductStatus(uint256 indexed productId, Stage stage);
+
+    modifier onlyOwner(uint256 _productId) {
+        require(
+            msg.sender == productDetailsMap[_productId].owner,
+            "Only product owner can call this function."
+        );
+        _;
+    }
 
     constructor() {
         rateChart[0] = 1000;
@@ -42,6 +51,38 @@ contract DistributedManufacturing {
         address machine5,
         address finished
     ) public payable returns (uint256) {
+        uint256 cost = calculateCost(not_started, machine1, machine2, machine3, machine4, machine5, finished);
+        require(msg.value >= cost, "Insufficient payment");
+
+        address[7] memory _addresses = [
+            not_started,
+            machine1,
+            machine2,
+            machine3,
+            machine4,
+            machine5,
+            finished
+        ];
+
+        productCounter++;
+        productDetailsMap[productCounter] = ProductDetails(
+            productCounter,
+            msg.sender,
+            _addresses,
+            Stage.not_started
+        );
+        return productCounter;
+    }
+
+    function calculateCost(
+        address not_started,
+        address machine1,
+        address machine2,
+        address machine3,
+        address machine4,
+        address machine5,
+        address finished
+    ) private view returns (uint256) {
         uint256 cost = 0;
         address[7] memory _addresses = [
             not_started,
@@ -58,16 +99,7 @@ contract DistributedManufacturing {
                 cost += rateChart[i - 1];
             }
         }
-
-        require(msg.value >= cost, "Insufficient payment");
-        productCounter++;
-        productDetailsMap[productCounter] = ProductDetails(
-            productCounter,
-            msg.sender,
-            _addresses,
-            Stage.not_started
-        );
-        return productCounter;
+        return cost;
     }
 
     function acceptOrder(uint256 _productId, uint256 machineIndex) public {
@@ -83,28 +115,30 @@ contract DistributedManufacturing {
         productDetailsMap[_productId].addresses[machineIndex] = msg.sender;
     }
 
-    function sendNextMachine(uint256 _productId) external payable returns (uint256)  {
-        
+        function sendNextMachine(uint256 _productId) external payable onlyOwner(_productId) returns (uint256) {
         ProductDetails storage product = productDetailsMap[_productId];
         require(product.stage != Stage.finished, "Product already finished");
-        
+
         uint256 currentStageIndex = uint256(product.stage);
-
         address payable senderAddress = payable(msg.sender);
-        address currentMachineAdress = product.addresses[currentStageIndex];
-        
-        require(senderAddress == currentMachineAdress, "Sender is not current machine owner" );
+        address currentMachineAddress = product.addresses[currentStageIndex];
 
-        if(senderAddress != product.owner) {
+        require(senderAddress == currentMachineAddress, "Sender is not current machine owner");
+
+        if (senderAddress != product.owner) {
             uint256 transferAmount = rateChart[currentStageIndex - 1];
-            require (transferAmount <= address(this).balance, "Insufficient contract balance");
+            require(transferAmount <= address(this).balance, "Insufficient contract balance");
             senderAddress.transfer(transferAmount);
         }
 
-        
-        
         product.stage = Stage(currentStageIndex + 1);
+        emit ProductStatus(_productId, product.stage);
 
         return address(this).balance;
     }
+
+    function getProductDetails(uint256 _productId) public view returns (ProductDetails memory) {
+        return productDetailsMap[_productId];
+    }
 }
+
